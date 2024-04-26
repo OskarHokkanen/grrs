2a9f1a016157f1ebe4a807101017a7d279ba3691
OskarHokkanen <hokkanen.oskar@gmail.com>
2024-04-26 09:55:33 +0200

    Implements better error handling

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,23 @@
+use anyhow::{Context, Result};
 use clap::Parser;
-use std::{fs, path};
 /// Search for a pattern in a file and display the lines that contai it.
 #[derive(Parser)]
 struct Cli {
     /// The pattern to look for
     pattern: String,
     /// The path to the file to read
-    path: path::PathBuf,
+    path: std::path::PathBuf,
 }
-// TODO: Nicer error reporting
-fn main() {
+
+fn main() -> Result<()> {
     let args = Cli::parse();
-    let content = fs::read_to_string(&args.path).expect("could not read file");
+    let content = std::fs::read_to_string(&args.path)
+        .with_context(|| format!("Could not read file `{}`", args.path.display()))?;
     for line in content.lines() {
         // TODO: Read about BufReader to not read the whole file (read_to_string()) into memory.
         if line.contains(&args.pattern) {
             println!("{}", line);
         }
     }
+    Ok(())
 }
